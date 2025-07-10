528e952d4b26dc5d78d1b841500adc287723085d
Alireza Sheikhi <Sheikhi.L.alireza@gmail.com>
2025-07-10 20:57:11 +0330

    Added the countries project

---
+++ 09-countries/src/components/CountriesList.jsx
@@ -0,0 +1,25 @@
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import CountryItem from "./CountryItem";
+import Message from "./Message";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return <Message message="Add your first city by clicking on the map" />;
+
+  const countries = cities.reduce((arr, cur) => {
+    if (arr.map((el) => el.country).includes(cur.country)) return [...arr];
+    else return [...arr, { country: cur.country, emoji: cur.emoji }];
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
